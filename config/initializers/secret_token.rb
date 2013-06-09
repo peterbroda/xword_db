43498cab5939f793767f0358c30e26bca6f2f0a6
peter <peter.bananarchy@gmail.com>
2013-06-09 22:25:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XwordDb::Application.config.secret_token = 'fb04bff8a8f201d3ca2cf44cfde1beae1947da8caa69297029d57781bda7f1fed07820484f6f8b28d0a12b7554893e1a6b2f508236954f8ed96eecf4fc05778a'
